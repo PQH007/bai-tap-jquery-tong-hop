870a88ec61eed7f4c9e4ed78b63bc8a6dec179be
PQHtobi <phanquochung2511@gmail.com>
2022-07-27 23:48:42 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+});
+$(':button').css({
+    'border': '1px solid black',
+    'border-radius': '5px',
+    'cursor': 'pointer',
+});
+
+$('#go1').click(function(){
+    var a = $('#sex','.radiosex');
+    // for(var i = 0; i < a.lenght; i ++);
+    console.log(a);
+    console.log(a.lenght);
+}); 
